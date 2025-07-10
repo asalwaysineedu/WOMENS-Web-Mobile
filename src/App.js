5119f50b8f7ecd111b4056152dc06ac641d3f148
V6NV5337 <92849618+asalwaysineedu@users.noreply.github.com>
2025-07-10 17:04:52 +0900

    add: 프로젝트 setting 추가 [WWM-1]
    1. tailwind 추가
    2. moment 추가
    3. 기타 등등 라이브러리 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import './assets/css/globals.css';
+import './assets/css/style.css';
+import './index.js';
+
+import Routes from './routes/routes';
 
 function App() {
-  return (
-    <></>
-  );
+  return <Routes />;
 }
 
 export default App;
---
+++ src/assets/images/index.js
@@ -0,0 +1,1 @@
+export const IMAGES_URL = `${process.env.PUBLIC_URL}/images`;
