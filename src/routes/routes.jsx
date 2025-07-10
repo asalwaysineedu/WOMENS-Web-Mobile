11e39076b1618cc9500bff7615211fb39107e543
V6NV5337 <92849618+asalwaysineedu@users.noreply.github.com>
2025-07-10 17:15:02 +0900

    chore: 사용자들이 소셜 로그인을 할 수 있는 로그인 페이지 UI를 구현합니다. [WWM-3]

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,5 +1,19 @@
+import React from 'react';
+import PrivateRoute from './PrivateRoute';
+import { ROUTES, ROUTES_BIG_KEY } from 'utils/const/routes';
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
+
+// 페이지 컴포넌트
 import MainPg from '../pages/main/MainPg';
+import LoginPg from 'pages/login/LoginPg';
 import SearchPg from 'pages/search/SearchPg';
+
+// 라우터 설정
+const router = createBrowserRouter([
+  // {
+  //   path: ROUTES_BIG_KEY.BIG_KEY,
+  //   element: <Navigate to={ROUTES.DASHBOARD} />,
+  // },
   {
     path: '/',
     /* 인증을 하지 않아야만 접속가능 페이지 (로그인 성공 시, 해당페이지 접속 불가) */
@@ -22,3 +36,35 @@ import SearchPg from 'pages/search/SearchPg';
       },
     ],
   },
+  {
+    path: '/login',
+    /* 인증을 하지 않아야만 접속가능 페이지 (로그인 성공 시, 해당페이지 접속 불가) */
+    element: <PrivateRoute authRouter={false} />,
+    children: [
+      {
+        path: ROUTES.LOGIN,
+        element: <LoginPg />,
+      },
+    ],
+  },
+  // {
+  //   path: '/',
+  //   /* 인증을 해야만 접속가능 페이지 (로그인후 accessToken이 브라우저에 저장되어 있는지 체크) */
+  //   element: <PrivateRoute authRouter />,
+  //   children: [
+  //     /* 대시보드 */
+  //     { path: ROUTES.DASHBOARD, element: <DashboardPg /> },
+
+  //   ],
+  // },
+  {
+    path: '*', // 모든 매칭되지 않는 경로를 404로 처리
+    // element: <NotFoundPage />,
+  },
+]);
+
+const Router = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default Router;
