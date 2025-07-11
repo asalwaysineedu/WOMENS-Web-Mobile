0e6f3fde359c816993c2f83c5f269b3d7b61a21a
V6NV5337 <92849618+asalwaysineedu@users.noreply.github.com>
2025-07-11 11:45:42 +0900

    feat: 페이지를 이동할 수 있는 Bottom 네비게이션 바를 구현합니다. [WWM-7]
    1. 일단 뼈대만 구축

--- src/components/LayoutCp.jsx
+++ src/components/LayoutCp.jsx
@@ -3,14 +3,16 @@ import HeaderLayoutCp from './header/layout/HeaderLayoutCp';
 import BodyLayoutCp from './body/layout/BodyLayoutCp';
 import BottomNavigationBarCp from './navigationBar/BottomNavigationBarCp';
 
-const LayoutCp = ({ children, header = '', bottomNav = '' }) => {
+const LayoutCp = ({ children, header = '', useBottomNavigationBar = false }) => {
   return (
     <>
-      {header && <HeaderLayoutCp>{header}</HeaderLayoutCp>}
-      <BodyLayoutCp useHeader={header ? true : false} useBottomNavigationBar={bottomNav ? true : false}>
-        {children}
-      </BodyLayoutCp>
-      {bottomNav && <BottomNavigationBarCp>{bottomNav}</BottomNavigationBarCp>}
+      <div className="">
+        {header && <HeaderLayoutCp>{header}</HeaderLayoutCp>}
+        <BodyLayoutCp useHeader={header ? true : false} useBottomNavigationBar={useBottomNavigationBar}>
+          {children}
+        </BodyLayoutCp>
+        {useBottomNavigationBar && <BottomNavigationBarCp />}
+      </div>
     </>
   );
 };
